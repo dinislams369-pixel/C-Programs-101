b020c861f11612f1e09a748f5494d53a9e511b16
Din Islam Sheikh <dinislam.s369@gmail.com>
2026-03-02 17:09:48 +0530

    Define integer, float, char, and double types
    Updated comments to clarify data types used. Here I showed using of data types and its format specifier and use of \n .

--- 03_ basic _data_type.c
+++ 03_ basic _data_type.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int age = 20;            // integer data type
-    float height = 5.8;      // floating-point data type
-    char grade = 'A';        // character data type
-    double salary = 25000.50; // double precision data type
+    int age = 20;            // int stores whole numbers
+    float height = 5.8;      // float data type stores decimal numbers
+    char grade = 'A';        // char data type stores character
+    double salary = 25000.50; // double stores large decimal values
             // \n moves the cursor to the next line 
            // %d, %f, %c,%f are format specifier of data types 
     printf("Age: %d\n", age); 
