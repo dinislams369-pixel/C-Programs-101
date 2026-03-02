75e00c793b4777954a84d375c971fcb0bc22f25e
Din Islam Sheikh <dinislam.s369@gmail.com>
2026-03-02 16:50:57 +0530

    Implement basic arithmetic operations in C
    This program illustrates the use of arithmetic operators in C
    programming. It performs basic operations such as addition,
    subtraction, multiplication, and division on numerical values
    and displays the computed results using the printf() function.

---
+++ 04_arithmetic_operations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main (){
+    float X,Y ;  // using float data type to sore decimal values 
+         printf("Enter valu of X and Y:");
+          scanf("%f %f",&X,&Y);
+            printf("Arithmetic Operations :\n");
+          printf("   Addition(+) =%.1f\n",X+Y);
+       printf("   Substraction (-) =%.1f\n",X-Y);
+     printf("   Multiplication (*) =%.1f\n",X*Y);
+   printf("   Division(/) =%.1f",X/Y);
+return 0;
+}
