3e10b9fc587af38f088c57f8494abde42787ca0e
Din Islam Sheikh <dinislam.s369@gmail.com>
2026-03-04 22:33:05 +0530

    Add for loop example to understand its syntax and execution
     This program demonstrates the use of the for loop in C programming.
    The for loop is used to execute a block of statements repeatedly
    for a specific number of iterations. In this program, the loop
    starts from 1 and continues up to 5, printing each value in
    sequence using the printf() function.

---
+++ 06_for_loop.c
@@ -0,0 +1,16 @@
+/* syntax of for loop
+for( initialization; condition; incriment)
+{
+  statement to print 
+  }*/
+
+
+
+#include <stdio.h>
+int main (){
+  for (int i=1; i<=5; i++)
+    {
+       printf("%d\n", i);
+    }
+return 0;
+}
